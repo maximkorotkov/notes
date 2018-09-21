8769b0e194e7bf8e16f680d688035147187b7608
Maxim Korotkov <maxyoutuber@mail.ru>
2018-09-21 22:37:16 +0300

    Mark selected note in list

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
+import { Session } from 'meteor/session';
 
 import Signup from '../ui/Signup';
 import Dashboard from '../ui/Dashboard';
@@ -12,6 +13,13 @@ const authenticatedPages = ['/dashboard'];
 const onEnterPublicPage = () => {
   if (Meteor.userId()) {
     browserHistory.replace('/dashboard');
+  }
+};
+const onEnterNotePage = (nextState) => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  } else {
+    Session.set('selectedNoteId', nextState.params.id);
   }
 };
 const onEnterPrivatePage = () => {
@@ -35,7 +43,7 @@ export const routes = (
     <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
     <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
     <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterPrivatePage}/>
+    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage}/>
     <Route path="*" component={NotFound}/>
   </Router>
 );
